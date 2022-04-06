260ba4e2040d7877b8ae38f12dbcadfc04b03eba
IanElu <roberto-rguezkof@hotmail.com>
2022-04-06 14:23:50 -0600

    version 1.0.5 worker fetch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const appVersion = "1.0.4";
+const appVersion = "1.0.5";
 const styleLog = {
   base: [
     "color: #fff",
@@ -107,14 +107,25 @@ self.addEventListener("activate", evt => {
     })
   )
 });
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-      caches.match(event.request)
-          .then(function (response) {
-              if (response) {
-                  return response;
-              }
-              return fetch(event.request);
-          })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async() => {
+    const cache = await caches.open(cacheName);
+    try {
+        const cachedResponse = await cache.match(event.request);
+        if(cachedResponse) {
+            console.log('cachedResponse: ', event.request.url);
+            return cachedResponse;
+        }
+        const fetchResponse = await fetch(event.request);
+        if(fetchResponse) {
+            console.log('fetchResponse: ', event.request.url);
+            await cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+        }
+    }   catch (error) {
+        console.log('Fetch failed: ', error);
+        const cachedResponse = await cache.match('/en/offline.html');
+        return cachedResponse;
+    }
+  })());
 });
